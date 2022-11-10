b150855e2acc52c2040ab6c5fe960ebf1d04459d
Aaron Pisacane <Pisacane@einstein.consulting>
2022-11-10 17:15:55 -0500

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "YOUR KEY HERE"
+
+# Geoapify API Key
+geoapify_key = "964b2709f925410792147cb65ea51909"
+
---
+++ config.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "f5e42526b163b823770ce5f18b017bc8"
+
+# Geoapify API Key
+geoapify_key = "e1cd18eefebe465f82fa4e9813d096a8"
+
+#Google API
+gkey = "AIzaSyDu9tLINExFZxOc7tE8elgdtsA1AGoNs5g"
